7cb54c1f71bb4dd86e020cc328918ca8c300dd1c
Vinill <danimuller758@gmail.com>
2023-02-21 21:55:14 -0300

    push 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edito si quiero
         </p>
         <a
           className="App-link"
@@ -18,6 +18,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <body>
+        <p>Hola</p>
+      </body>
     </div>
   );
 }
